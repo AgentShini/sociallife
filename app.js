8d80f2af52655d4d53845c59dcf18a4646ee275f
AgentShini <mayowaakande73@gmail.com>
2023-10-08 01:21:16 +0100

    Cleaned up Codebase

--- app.js
+++ app.js
@@ -3,15 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var studentRoute = require("./routes/student")
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var bodyparser = require('body-parser')
 var mongoose = require('mongoose')
-
-
 var app = express();
+
 mongoose.connect("mongodb://localhost:27017/test")
 mongoose.connection.on('connected',(err)=>{
 if(err){
@@ -20,19 +14,12 @@ if(err){
 })
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/student",studentRoute)
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
